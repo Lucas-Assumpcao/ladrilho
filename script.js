ae91ead9a4e2b8fd965e828f035db759b0485400
Lucas-Assumpcaogit config --global user.email assumpdev@gmail.comgit config --global user.name Lucas-Assumpcao <assumpdev@gmail.com>
2026-05-11 20:19:51 -0300

    ladrilho

---
+++ script.js
@@ -0,0 +1,28 @@
+const url = `https://picsum.photos/v2/list`;
+const body = document.querySelector('body');
+const main = document.querySelector('main');
+
+//criação de função assíncrona para buscar os dados da url
+async function getDados(url) {
+    const dados = await (await fetch(url)).json();
+//variavel para armazenar os dados do fetch
+//O primeiro await é para esperar a resposta do fetch, e o segundo await é para esperar a conversão da resposta em json    
+    filtarDdados(dados);
+} 
+
+//iniciando a função getDados e passa a url como parâmetro de busca.
+getDados(url);
+
+
+// funcao para filtrar os dados e mostrar as urls das imagens no console
+function filtarDdados(dados) {
+    const urlIMG = dados.forEach((elemento) => {
+        console.log(elemento.url)
+    });
+}
+//Função apra estilizar o os elementos da página (body e main)
+function estilizarMural(){
+    body.className = "flex items-center justify-center"
+}   main.classList.add("columns-3", "gap-5", "*:mt-5")
+
+estilizarMural();
